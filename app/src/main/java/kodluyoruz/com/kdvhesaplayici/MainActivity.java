e5d8f0947203ec1ade079c8a92bb179e5d8236a4
GokhanKorkmaz <gokankorkmaz@gmail.com>
2017-10-21 21:44:52 +0300

    RadioButton added

--- app/src/main/java/kodluyoruz/com/kdvhesaplayici/MainActivity.java
+++ app/src/main/java/kodluyoruz/com/kdvhesaplayici/MainActivity.java
@@ -5,16 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
+import static android.R.attr.x;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button activity_main_btnKdv1, activity_main_btnKdv8, activity_main_btnKdv18, activity_main_btnKdvHaric, activity_main_btnKdvDahil;
+    Button activity_main_btnKdv1, activity_main_btnKdv8, activity_main_btnKdv18;
     EditText activity_main_etKdvTutari;
     TextView activity_main_tvkdvHaric, activity_main_tvkdvTutari, activity_main_tvKdv;
     String editControl;
-
-    @Override
+    RadioButton activity_main_rbtnKdvDahil, activity_main_rbtnKdvHaric;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -23,8 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         activity_main_btnKdv1.setOnClickListener(this);
         activity_main_btnKdv8.setOnClickListener(this);
         activity_main_btnKdv18.setOnClickListener(this);
-        activity_main_btnKdvDahil.setOnClickListener(this);
-        activity_main_btnKdvHaric.setOnClickListener(this);
+        activity_main_rbtnKdvDahil.setOnClickListener(this);
+        activity_main_rbtnKdvHaric.setOnClickListener(this);
     }
 
     private void initView() {
@@ -32,8 +38,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         activity_main_btnKdv8 = (Button) findViewById(R.id.activity_main_btnKdv8);
         activity_main_btnKdv18 = (Button) findViewById(R.id.activity_main_btnKdv18);
 
-        activity_main_btnKdvHaric = (Button) findViewById(R.id.activity_main_btnKdvHaric);
-        activity_main_btnKdvDahil = (Button) findViewById(R.id.activity_main_btnKdvDahil);
+        activity_main_rbtnKdvDahil = (RadioButton) findViewById(R.id.activity_main_rbtnKdvDahil);
+        activity_main_rbtnKdvHaric = (RadioButton) findViewById(R.id.activity_main_rbtnKdvHaric);
 
         activity_main_etKdvTutari = (EditText) findViewById(R.id.activity_main_etKdvTutari);
 
@@ -41,109 +47,104 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         activity_main_tvkdvTutari = (TextView) findViewById(R.id.activity_main_tvkdvTutari);
         activity_main_tvKdv = (TextView) findViewById(R.id.activity_main_tvKdv);
 
-
-    }
+     }
 
     @Override
     public void onClick(View v) {
 
 
+
+
         switch (v.getId()) {
             case R.id.activity_main_btnKdv1:
+                editControl = activity_main_etKdvTutari.getText().toString();
+                if (editControl.matches("")) {
+                    Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
+                if (activity_main_rbtnKdvDahil.isChecked()) {
+                    activity_main_tvKdv.setText("KDV HARİÇ");
 
-                if(v.getId()==R.id.activity_main_btnKdvDahil){
-                    editControl = activity_main_etKdvTutari.getText().toString();
-                    if (editControl.matches("")) {
-                        Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else {
-                        int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
-                        Double sonuc = (sayi / 1.01);
-                        activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
-                        activity_main_tvkdvTutari.setText(sonuc + " ");
-                    }
-                }else if(v.getId()==R.id.activity_main_btnKdvHaric){
-                    editControl = activity_main_etKdvTutari.getText().toString();
-                    if (editControl.matches("")) {
-                        Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
-                        return;
-                    } else {
-                        int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
-                        Double sonuc = (sayi * 0.01);
-                        activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
-                        activity_main_tvkdvTutari.setText(sonuc + " ");
-                    }
+                    int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
+                    Double sonuc = (sayi / 1.01);
+                    double son=(Math.floor(sonuc* 100) / 100);
+                    activity_main_tvkdvHaric.setText(son + " ");
+                    activity_main_tvkdvTutari.setText(new DecimalFormat("##.##").format(sayi-son));
+
+                   // activity_main_tvkdvTutari.setText(sayi-son+" ");
+
+                } else if (activity_main_rbtnKdvHaric.isChecked()) {
+                    activity_main_tvKdv.setText("KDV DAHİL");
+
+                    int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
+                    Double sonuc = (sayi * 0.01);
+                    activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
+                    activity_main_tvkdvTutari.setText(sonuc + " ");
+
 
 
                 }
-
-
-
-
-
+break;
 
             case R.id.activity_main_btnKdv8:
-
-                switch (v.getId()) {
-                    case R.id.activity_main_btnKdvDahil:
-                        editControl = activity_main_etKdvTutari.getText().toString();
-                        if (editControl.matches("")) {
-                            Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
-                            return;
-                        } else {
-                            int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
-                            Double sonuc = (sayi / 1.08);
-                            activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
-                            activity_main_tvkdvTutari.setText(sonuc + " ");
-                        }
-                        break;
-
-
-                    case R.id.activity_main_btnKdvHaric:
-                        editControl = activity_main_etKdvTutari.getText().toString();
-                        if (editControl.matches("")) {
-                            Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
-                            return;
-                        } else {
-                            int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
-                            Double sonuc = (sayi * 0.08);
-                            activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
-                            activity_main_tvkdvTutari.setText(sonuc + " ");
-                        }
-                        break;
+                editControl = activity_main_etKdvTutari.getText().toString();
+                if (editControl.matches("")) {
+                    Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                if (activity_main_rbtnKdvDahil.isChecked()) {
+                    activity_main_tvKdv.setText("KDV HARİÇ");
+
+                    int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
+                    Double sonuc = (sayi / 1.08);
+                    double son=(Math.floor(sonuc* 100) / 100);
+                    activity_main_tvkdvHaric.setText(son + " ");
+                    activity_main_tvkdvTutari.setText(new DecimalFormat("##.##").format(sayi-son));
+
+                } else if (activity_main_rbtnKdvHaric.isChecked()) {
+                    activity_main_tvKdv.setText("KDV DAHİL");
+
+                    int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
+                    Double sonuc = (sayi * 0.08);
+                    activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
+                    activity_main_tvkdvTutari.setText(sonuc + " ");
+
+
+
+                }
+
+
                 break;
             case R.id.activity_main_btnKdv18:
-
-                switch (v.getId()) {
-                    case R.id.activity_main_btnKdvDahil:
-                        editControl = activity_main_etKdvTutari.getText().toString();
-                        if (editControl.matches("")) {
-                            Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
-                            return;
-                        } else {
-                            int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
-                            Double sonuc = (sayi / 1.18);
-                            activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
-                            activity_main_tvkdvTutari.setText(sonuc + " ");
-                        }
-                        break;
-
-
-                    case R.id.activity_main_btnKdvHaric:
-                        editControl = activity_main_etKdvTutari.getText().toString();
-                        if (editControl.matches("")) {
-                            Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
-                            return;
-                        } else {
-                            int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
-                            Double sonuc = (sayi * 0.18);
-                            activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
-                            activity_main_tvkdvTutari.setText(sonuc + " ");
-                        }
-                        break;
+                editControl = activity_main_etKdvTutari.getText().toString();
+                if (editControl.matches("")) {
+                    Toast.makeText(this, "Lütfen Tutar Giriniz", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                if (activity_main_rbtnKdvDahil.isChecked()) {
+                    activity_main_tvKdv.setText("KDV HARİÇ");
+
+                    int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
+                    Double sonuc = (sayi / 1.18);
+                    double son=(Math.floor(sonuc* 100) / 100);
+                    activity_main_tvkdvHaric.setText(son + " ");
+                    activity_main_tvkdvTutari.setText(new DecimalFormat("##.##").format(sayi-son));
+
+                } else if (activity_main_rbtnKdvHaric.isChecked()) {
+                    activity_main_tvKdv.setText("KDV DAHİL");
+                    int sayi = Integer.parseInt(activity_main_etKdvTutari.getText().toString());
+                    Double sonuc = (sayi * 0.18);
+                    activity_main_tvkdvHaric.setText(sayi + sonuc + " ");
+                    activity_main_tvkdvTutari.setText(sonuc + " ");
+
+
+
+                }
+
+
                 break;
         }
 
@@ -153,3 +154,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 }
 
 
+//TODO RadioButtonları üste al
